935c9c103f7285f6dc9a0e642c459b27722024b1
YannikBauer1 <yannikbauer.1@gmail.com>
2024-10-24 17:44:38 -0600

    cleaning cateogry and coutnry

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ df = pd.read_csv('data.csv')
 #print(df.shape)
 
 # return me the data types of each column
-#print(df.dtypes)
+print(df.dtypes)
 
 # remove rows that have no name in it ( false rows )
-print(df['competitors_name'].isnull().sum())
-print("")
+#print(df['competitors_name'].isnull().sum())
+#print("")
 df = df.dropna(subset=['competitors_name'])
 
 # in columns judging, finals, total, replace the value of - and NS and DQ with NaN
@@ -34,17 +34,17 @@ df['finals'] = np.where(df['finals'].isnull() & df['judging'].notnull(), 0, df['
 df['check'] = df['judging'] + df['finals'] == df['total']
 
 # print how many falses are in the check column
-print(df['check'].value_counts())
-print("")
+#print(df['check'].value_counts())
+#print("")
 
 # show me the rows that have false in the check column, show the columns judging, finals, total and competitors_name which have not nathing at the place column
-print(df[(df['check'] == False) & (df['place'].notnull())][['judging', 'finals', 'total', 'place', 'competitors_name']])
-print("")
+#print(df[(df['check'] == False) & (df['place'].notnull())][['judging', 'finals', 'total', 'place', 'competitors_name']])
+#print("")
 #print(df[df['check'] == False][['judging', 'finals', 'total', 'place', 'competitors_name']])
 
 indexes = df[(df['check'] == False) & (df['place'].notnull())].index
-print(indexes)
-print("")
+#print(indexes)
+#print("")
 
 #for index in indexes:
 #    start = max(index - 4, 0)
@@ -60,5 +60,65 @@ df.loc[3404, 'judging'] = 9.0
 df.loc[3689, 'total'] = 17.0
 
 df['check'] = df['judging'] + df['finals'] == df['total']
-print(df[(df['check'] == False) & (df['place'].notnull())][['judging', 'finals', 'total', 'place', 'competitors_name']])
-print("")
+#print(df[(df['check'] == False) & (df['place'].notnull())][['judging', 'finals', 'total', 'place', 'competitors_name']])
+#print("")
+
+
+# rename the column competition_type into category
+df = df.rename(columns={'competition_type': 'category'})
+
+# make all the values of the category column uppercase
+df['category'] = df['category'].str.upper()
+
+# if " - OPEN" is in the category column, remove it
+df['category'] = df['category'].str.replace(' - OPEN', '')
+
+# if category is "BIKINI" replace it with "WOMEN'S BIKINI"
+df['category'] = df['category'].replace('BIKINI', 'WOMEN\'S BIKINI')
+# if category is "CLASSIC PHYSIQUE" replace it with "MEN'S CLASSIC PHYSIQUE"
+df['category'] = df['category'].replace('CLASSIC PHYSIQUE', 'MEN\'S CLASSIC PHYSIQUE')
+
+
+# Filter out rows with "MASTERS" or "WHEELCHAIR" in the category column
+df_filtered = df[~df['category'].str.contains("WHEELCHAIR")]
+df_filtered = df_filtered[~df_filtered['category'].str.contains("MASTERS")]
+
+category_translation = {
+    "MEN'S 212 BODYBUILDING": "Men's 212 Bodybuilding",
+    "MEN'S BODYBUILDING": "Men's Bodybuilding",
+    "MEN'S CLASSIC PHYSIQUE": "Men's Classic Physique",
+    "MEN'S PHYSIQUE": "Men's Physique",
+    "WOMEN'S BIKINI": "Women's Bikini",
+    "WOMEN'S BODYBUILDING": "Women's Bodybuilding",
+    "WOMEN'S FIGURE": "Women's Figure",
+    "WOMEN'S FITNESS": "Women's Fitness",
+    "WOMEN'S PHYSIQUE": "Women's Physique",
+    "WOMEN'S WELLNESS": "Women's Wellness"
+}
+# Translate the categories using the dictionary
+df_filtered['category'] = df_filtered['category'].map(category_translation)
+
+
+# show me how many unique values are in the category column
+#unique_categories = sorted(df_filtered['category'].unique())
+#for category in unique_categories:
+#    print(category)
+
+
+def parse_country(entry):
+    parts = entry.split(',')
+    if len(parts) == 2:
+        city = parts[0].strip() if parts[0].strip() else ""
+        state = parts[1].strip()
+        country = 'United States'
+    else:
+        city = ""
+        state = ""
+        country = parts[0].strip()
+    return {'city': city, 'state': state, 'country': country}
+
+# Apply the function to the "country" column
+df_filtered['country'] = df_filtered['country'].apply(parse_country)
+
+# print the columns country as a list
+print(df_filtered['country'].tolist())
