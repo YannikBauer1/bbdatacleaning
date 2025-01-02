25c33f5bc20e07c514cf818643089df0b6e2d3ce
YannikBauer1 <yannikbauer.1@gmail.com>
2025-01-02 17:35:06 +0100

    create competition and create results

--- request.py
+++ request.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import json
 import ast
+import re
 
 import os
 from supabase import create_client, Client
@@ -12,6 +13,10 @@ os.environ["SUPABASE_KEY"] = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzd
 url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
+
+response = supabase.auth.sign_in_with_password(
+    {"email": "yannikbauer.1@gmail.com", "password": "Weisnet1"}
+)
 
 def createAthletes(df):
   for index, row in df.iterrows():
@@ -30,30 +35,83 @@ def createAthletes(df):
           updatedPerson = supabase.table("person").update({"athlete_id": createdAthlete.data[0]["id"]}).eq("id", createdPerson.data[0]["id"]).execute()
           print(person_name)
 
+def format_date(date_str):
+    if pd.isna(date_str):
+        return None
+    return pd.to_datetime(date_str, format="%d.%m.%Y").isoformat()
+
 def createCompetition(df):
-  for index, row in df.iterrows():
-      print(index)
-      person_name = row["competitors_name"].replace(",", "")
-      country = row["country"]
-      persons = supabase.table("person").select("*").or_(f"name.eq.{person_name},name_long.eq.{person_name}").execute().data
-      persons = [person for person in persons if person["birthplace"] == country]
-      category = supabase.table("category").select("*").eq("name", row["category"]).execute().data
-      if len(category) == 0:
-         print("Category not found")
-         exit()
-      if len(persons) == 0:
-          createdPerson = supabase.table("person").insert({"name": person_name, "name_long": person_name, "birthplace": row["country"], "nationality": [row["country"]["country"]]}).execute()
-          createdAthlete = supabase.table("athlete").insert({"name": person_name, "active": True, "category_id": category[0]["id"]}).execute()
-          updatedPerson = supabase.table("person").update({"athlete_id": createdAthlete.data[0]["id"]}).eq("id", createdPerson.data[0]["id"]).execute()
-          print(person_name)
+    for index, row in df.iterrows():
+        print(index)
+        name = row["name"]
+        eventName = row["eventName"]
+        competitions = supabase.table("competition").select("*").or_(f"name_short.eq.{name},name_long.eq.{name},name_key.eq.{name}").execute().data
+        competitionEvents = supabase.table("competitionEvent").select("*").or_(f"name.eq.{eventName},name_key.eq.{eventName}").execute().data
+        start_date = format_date(row["start_date"])
+        end_date = format_date(row["end_date"])
+        if pd.notna(row["promoter_website"]):
+          socials = {"website": row["promoter_website"]}
+        else:
+          socials = {}
+        if pd.notna(row["promoter"]):
+          promoter = row["promoter"]
+        else:
+          promoter = ""
+        if len(competitions) == 0 and len(competitionEvents) == 0:
+            createdCompetition = supabase.table("competition").insert({"name_key": name, "name_short": name, "name_long": name, "organization": row["comp_type"], "socials": socials}).execute()
+            createdCompetitionEvent = supabase.table("competitionEvent").insert({"name_key": name, "location": row["location"], "start_date": start_date ,"end_date":end_date, "name": eventName, "promoter": promoter, "competition_id": createdCompetition.data[0]["id"]}).execute()
+            print(name, eventName)
+        elif len(competitionEvents) == 0:
+            createdCompetitionEvent = supabase.table("competitionEvent").insert({"name_key": name, "location": row["location"], "start_date": start_date ,"end_date":end_date, "name": eventName, "promoter": promoter, "competition_id": competitions[0]["id"]}).execute()
+            print(eventName)
+
+def createResults(df):
+    for index, row in df.iterrows():
+        print(index)
+        competition = row["competition"].replace('-', ' ').title()
+        
+        category = supabase.table("category").select("*").eq("name", row["category"]).execute().data
+        competitionEvent = supabase.table("competitionEvent").select("*").or_(f"name.eq.{competition},name_key.eq.{competition}").execute().data
+        eventCategories = supabase.table("eventCategory").select("*").eq("competitionEvent_id", competitionEvent[0]["id"]).eq("category_id", category[0]["id"]).execute().data
+
+        if len(eventCategories) == 0:
+            createdEventCategory = supabase.table("eventCategory").insert({"competitionEvent_id": competitionEvent[0]["id"], "category_id": category[0]["id"]}).execute()
+            eventCategory_id = createdEventCategory.data[0]["id"]
+            print(row["category"])
+        else:
+            eventCategory_id = eventCategories[0]["id"]
+
+        person_name = row["competitors_name"].replace(",", "")
+        country = row["country"]
+        persons = supabase.table("person").select("*").or_(f"name.eq.{person_name},name_long.eq.{person_name}").execute().data
+        persons = [person for person in persons if person["birthplace"] == country]
+        athlete_id = persons[0]["athlete_id"]
+
+        results = supabase.table("result").select("*").eq("eventCategory_id", eventCategory_id).eq("athlete_id", athlete_id).execute().data
+
+        if len(results) == 0:
+            createdResult = supabase.table("result").insert({
+                "eventCategory_id": eventCategory_id, 
+                "athlete_id": athlete_id, 
+                "prejudging": int(float(row["judging"])) if pd.notna(row["judging"]) else None, 
+                "finals": int(float(row["finals"])) if pd.notna(row["finals"]) else None, 
+                "total": int(float(row["total"])) if pd.notna(row["total"]) else None, 
+                "place": int(float(row["place"])) if pd.notna(row["place"]) else None
+            }).execute()
+            print(person_name)
+
+#df = pd.read_csv('data_clean/2024/tables_2.csv')
+#df['country'] = df['country'].apply(ast.literal_eval)
+
+#df = pd.read_csv('data_clean/sidebar/2024.csv')
+#df['location'] = df['location'].apply(ast.literal_eval)
 
 df = pd.read_csv('data_clean/2024/tables_2.csv')
-
-# change the type of the column country to be an object
 df['country'] = df['country'].apply(ast.literal_eval)
 
-# print the number of rows in the df
 print(df.shape)
-
-
 #createAthletes(df)
+#createCompetition(df)
+createResults(df)
+
+response = supabase.auth.sign_out()
